bd46db418387f9ff60a893b998e0ae3b66fff5d5
mihirWeb <mihirpratapsingh166@gmail.com>
2024-07-04 15:19:16 +0530

    Navbar and announcement added

---
+++ src/components/Announcements.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    font-weight: 500;
+`
+
+const Announcements = () => {
+  return (
+    <Container>
+      Raste ka maal saste mai !!!!!
+    </Container>
+  )
+}
+
+export default Announcements
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,91 @@
+import { Search, ShoppingCartOutlined } from '@mui/icons-material';
+import React from 'react';
+import styled from '@emotion/styled';
+import { Badge } from '@mui/material';
+
+const Container = styled.div`
+    height: 60px;
+    
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: Center 
+
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: Center;
+`
+const Center = styled.div`
+    flex: 1;
+    text-align: center;
+`
+const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: Center;
+    margin-left: 25px;
+    padding: 5px
+`
+
+const Input = styled.input`
+  border: none;
+`
+
+const Logo = styled.div`
+  font-weight: bold;
+`
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  margin-left: 25px;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+            <Language>EN</Language>
+            <SearchContainer>
+            <Input />
+            <Search style={{color: "grey", fontSize: 16}}/>
+            </SearchContainer>
+        </Left>
+
+        <Center>
+          <Logo>UrbanDEN.</Logo>
+        </Center>
+        
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color='primary'>
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
